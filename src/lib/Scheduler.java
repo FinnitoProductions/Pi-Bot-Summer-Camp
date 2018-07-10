2946374c6fabdd5079b33e80825cade9e6877196
bokken12 <stopwatchj@gmail.com>
2018-07-10 12:58:12 -0700

    Fix Scheduler
    Prevent Scheduler from entering infinite loops when attempting to add commands.

--- src/lib/Scheduler.java
+++ src/lib/Scheduler.java
@@ -18,61 +18,122 @@ public class Scheduler {
 	private Set<Command> runningCommands;
 	private Stack<Command> toRemove;
 	
+	/**
+	 * Creates a new scheduler, with the internal collection as an identity set
+	 * wrapped around an IdentityHashMap. This allows for multiple similar
+	 * commands to run and count as different.
+	 */
 	private Scheduler() {
 		runningCommands = Collections.newSetFromMap(new IdentityHashMap<>());
 	}
 	
+	/**
+	 * @return the singleton instance of the scheduler
+	 */
 	public static Scheduler getInstance() {
 		if(sch == null)
 			sch = new Scheduler();
 		return sch;
 	}
 	
+	/**
+	 * Starts a new command running, stopping any commands that it would
+	 * conflict with
+	 * 
+	 * @param c
+	 *            the command to run
+	 */
 	public void add(Command c) {
 		if(c == null)
 			new IllegalArgumentException("Cannot start a null command").printStackTrace();
-		if(runningCommands.add(c))
-			new IllegalArgumentException("Command " + c.getClass().getSimpleName() + " is already running").printStackTrace();
-		for(Command o : runningCommands)
-			if(sharedSubsystem(c, o))
-				toRemove.push(o);
-		while(!toRemove.isEmpty())
-			remove(toRemove.pop(), true);
-		try {
-			c.initialize();
-		} catch(Exception e) {
-			System.err.println("Error initializing command " + c.getClass().getSimpleName());
-			e.printStackTrace();
+		else if(runningCommands.add(c))
+			new IllegalArgumentException("Command " + c.getClass().getSimpleName() + " is already running")
+					.printStackTrace();
+		else {
+			for(Command o : runningCommands)
+				if(c != o && sharedSubsystem(c, o))
+					toRemove.push(o);
+			while(!toRemove.isEmpty())
+				remove(toRemove.pop(), c);
+			try {
+				c.initialize();
+			} catch(Exception e) {
+				System.err.println("Error initializing command " + c.getClass().getSimpleName());
+				e.printStackTrace();
+			}
 		}
 	}
 	
+	/**
+	 * Stops a command from running (no interrupt), restarting all default
+	 * commands from subsystems used
+	 * 
+	 * @param c
+	 *            the command to stop
+	 */
 	public void remove(Command c) {
-		remove(c, false);
+		if(c == null)
+			new IllegalArgumentException("Cannot stop a null command").printStackTrace();
+		else if(!runningCommands.remove(c))
+			new IllegalArgumentException("Command " + c.getClass().getSimpleName() + " is not running")
+					.printStackTrace();
+		else {
+			try {
+				c.end();
+			} catch(Exception e) {
+				System.err.println("Error ending command " + c.getClass().getSimpleName());
+				e.printStackTrace();
+			}
+			for(Subsystem s : c.getRequired())
+				if(s.getDefaultCommand() != null)
+					add(s.getDefaultCommand());
+		}
 	}
 	
-	public void remove(Command c, boolean interrupt) {
-		if(c == null) {
-			new IllegalArgumentException("Cannot stop a null command").printStackTrace();;
-		}
-		if(!runningCommands.remove(c))
-			new IllegalArgumentException("Command " + c.getClass().getSimpleName() + " is not running").printStackTrace();;
-		try {
-			if(interrupt)
+	/**
+	 * Stops a command from running (interrupt), restarting all default commands
+	 * from subsystems used that don't conflict with the interruption
+	 * 
+	 * @param c
+	 *            the command to stop
+	 */
+	public void remove(Command c, Command cause) {
+		if(c == null)
+			new IllegalArgumentException("Cannot stop a null command").printStackTrace();
+		else if(!runningCommands.remove(c))
+			new IllegalArgumentException("Command " + c.getClass().getSimpleName() + " is not running")
+					.printStackTrace();
+		else {
+			try {
 				c.interrupted();
-			c.end();
-		} catch(Exception e) {
-			System.err.println("Error ending command " + c.getClass().getSimpleName());
-			e.printStackTrace();
+			} catch(Exception e) {
+				System.err.println("Error ending command " + c.getClass().getSimpleName());
+				e.printStackTrace();
+			}
+			for(Subsystem s : c.getRequired())
+				if(s.getDefaultCommand() != null)
+					if(!sharedSubsystem(cause, s.getDefaultCommand()))
+						add(s.getDefaultCommand());
 		}
-		for(Subsystem s : c.getRequired())
-			if(s.getDefaultCommand() != null)
-				add(s.getDefaultCommand());
 	}
 	
+	/**
+	 * Checks if a command is running
+	 * 
+	 * @param c
+	 *            the command to check
+	 * @return whether that command is contained in the set of running commands
+	 */
 	public boolean isRunning(Command c) {
 		return runningCommands.contains(c);
 	}
 	
+	/**
+	 * Executes each command currently running once, then removes them if they
+	 * have finished.
+	 * 
+	 * A single subsystem will never be executed more than once in a call of run
+	 */
 	public void run() {
 		for(Command c : runningCommands) {
 			try {
@@ -85,11 +146,7 @@ public class Scheduler {
 			}
 		}
 		while(!toRemove.isEmpty())
-			remove(toRemove.pop(), false);
-	}
-	
-	public void addSubsystem(Subsystem s) {
-		add(s.getDefaultCommand());
+			remove(toRemove.pop());
 	}
 	
 	/**
