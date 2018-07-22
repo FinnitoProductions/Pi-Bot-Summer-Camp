3a47d2e915673f2cadc4c60bfc613ab61d428474
FinnitoProductions <finn@teachmy.com>
2018-07-22 12:02:20 -0700

    Added concrete getVelocity() to FeedbackSensor

--- src/lib/FeedbackSensor.java
+++ src/lib/FeedbackSensor.java
@@ -5,7 +5,26 @@ package lib;
  * @author Finn Frankis
  * @version Jul 22, 2018
  */
-public interface FeedbackSensor
+public abstract class FeedbackSensor
 {
+    private double previousPosition;
+    private double previousTime;
+   
     public abstract double getPosition();
+    
+    public double getVelocity()
+    {
+        double currentPosition = getPosition(), currentTime = System.currentTimeMillis();
+        Double deltaT = currentTime - previousTime;
+        
+        if (previousTime == -1 || deltaT.equals(0))
+            return 0;
+        
+        double velocity = (currentPosition - previousPosition) / (currentTime - previousTime);
+        
+        previousPosition = currentPosition; 
+        previousTime = currentTime;
+        
+        return velocity;
+    }
 }
