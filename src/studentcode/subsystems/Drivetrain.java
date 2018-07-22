62b613d14fd544a54188dba0071c4fb1254672dc
FinnitoProductions <finn@teachmy.com>
2018-07-22 12:21:46 -0700

    Documented Drivetrain and added getter methods

--- src/studentcode/subsystems/Drivetrain.java
+++ src/studentcode/subsystems/Drivetrain.java
@@ -9,7 +9,7 @@ import studentcode.commands.*;
 import studentcode.robot.RobotMap;
 
 /**
- * 
+ * Represents the drivetrain on the robot.
  * @author Finn Frankis
  * @version Jul 8, 2018
  */
@@ -19,6 +19,9 @@ public class Drivetrain extends Subsystem
     private TalonSRX leftTalon;
     private TalonSRX rightTalon;
     
+    /**
+     * Constructs a new Drivetrain.
+     */
     public Drivetrain()
     {
         leftTalon = new TalonSRX(RobotMap.LEFT_MOTOR_ENABLE, 
@@ -30,23 +33,51 @@ public class Drivetrain extends Subsystem
     }
     
     /**
-    * 
+    * Sets up the default command for this subsystem (the one which the subsystem will default to 
+    * when no other commands are being sent).
     */
     public void initDefaultCommand()
     {
         setDefaultCommand(new DriveWithVelocityKeyboard());
     }
     
+    /**
+     * Drives the robot with a given speed and turn.
+     * @param speed the speed at which the robot is to be driven
+     * @param turn the amount by which the robot should turn
+     */
     public void arcadeDriveVelocity(double speed, double turn)
     {
         leftTalon.set(ControlMode.PercentOutput, speed, DemandType.FeedForward, +turn);
         rightTalon.set(ControlMode.PercentOutput, speed, DemandType.FeedForward, -turn);
     }
 
+    /**
+     * Gets this singleton instance of Drivetrain.
+     * @return the instance of Drivetrain
+     */
     public static Drivetrain getInstance()
     {
         if (dt == null)
             dt = new Drivetrain();
         return dt;
+    }
+    
+    /**
+     * Gets the left Talon on the Drivetrain.
+     * @return the left Talon
+     */
+    public TalonSRX getLeftTalon()
+    {
+        return leftTalon;
+    }
+    
+    /**
+     * Gets the right Talon on the Drivetrain.
+     * @return the right Talon
+     */
+    public TalonSRX getRightTalon()
+    {
+        return rightTalon;
     }
 }
