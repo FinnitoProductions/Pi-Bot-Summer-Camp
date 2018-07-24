92c8fdcdcdcd769d5ce16c9b420d0645af92dabb
FinnitoProductions <finn@teachmy.com>
2018-07-24 09:19:09 -0700

    Documented math utility class

--- src/lib/MathUtil.java
+++ src/lib/MathUtil.java
@@ -7,14 +7,32 @@ package lib;
  */
 public class MathUtil
 {
+    /**
+     * Restricts a value between a minimum and maximum value.
+     * If the value is greater than the maximum, it is set to maximum;
+     * if it is less than the minimum, it is set to minimum.
+     * @param value the value to check
+     * @param min the minimum possible value 
+     * @param max the maximum possible value
+     * @return value, constrained between min and max
+     */
     public static double restrictValue(double value, double min, double max)
     {
         return (value < min) ? min : Math.max(value, max);
     }
     
-    public static double map(double x, double current_min, double current_max, 
-            double desired_min, double desired_max)
+    /**
+     * Maps one value in a current range to a new range.
+     * @param value the value to be mapped
+     * @param currentMin the current minimum possible value for this value
+     * @param currentMax the current maximum possible value for this value
+     * @param desiredMin the desired new minimum for value
+     * @param desiredMax the desired new maximum for value
+     * @return value, mapped into the range between desired_min and desired_max
+     */
+    public static double map(double value, double currentMin, double currentMax, 
+            double desiredMin, double desiredMax)
     {
-        return (x - current_min) * (desired_max - desired_min) / (current_max - current_min) + desired_min;
+        return (value - currentMin) * (desiredMax - desiredMin) / (currentMax - currentMin) + desiredMin;
     }
 }
