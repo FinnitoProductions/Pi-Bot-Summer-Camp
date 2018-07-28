b318fe7a4ab6051f44c5cc6b318a4aa5e058c843
FinnitoProductions <finn@teachmy.com>
2018-07-28 18:08:58 +0200

    Modified teleoperated time for easier debugging

--- src/studentcode/robot/RobotMap.java
+++ src/studentcode/robot/RobotMap.java
@@ -8,7 +8,7 @@ package studentcode.robot;
 public class RobotMap
 {
     public static final int AUTON_TIME_MS = 15000;
-    public static final int TELEOP_TIME_MS = 135000;
+    public static final int TELEOP_TIME_MS = Integer.MAX_VALUE;//135000;
     
     public static final int LEFT_MOTOR_ENABLE = 4; //#32
     public static final int LEFT_MOTOR_FORWARD = 13; //#23
@@ -18,13 +18,13 @@ public class RobotMap
     public static final int RIGHT_MOTOR_FORWARD = 12; //10 
     public static final int RIGHT_MOTOR_BACKWARD = 14; //21
     
-    public static final int LEFT_ENCODER_ORANGE = 0;
-    public static final int LEFT_ENCODER_BROWN = 2;
-    public static final int RIGHT_ENCODER_ORANGE = 27;
-    public static final int RIGHT_ENCODER_BROWN = 28;
+    public static final int LEFT_ENCODER_ORANGE = 0; // BCM 17
+    public static final int LEFT_ENCODER_BROWN = 2; // BCM 27
+    public static final int RIGHT_ENCODER_ORANGE = 27; // BCM 16
+    public static final int RIGHT_ENCODER_BROWN = 28; // BCM 20
     
-    public static final int ARM_SERVO_GPIO = 23; // BCM 23
-    public static final int CLAW_SERVO_GPIO = 26;
+    public static final int ARM_SERVO_GPIO = 23; // BCM 13
+    public static final int CLAW_SERVO_GPIO = 26; // BCM 12
     
     public static final double MAX_SPEED = 0.08;
     public static final double MAX_TURN = 0.07;
